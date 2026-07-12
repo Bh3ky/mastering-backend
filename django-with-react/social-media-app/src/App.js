a4fda31fb9aef75988038ebfa104f02d4981df20
Bh3ky <127692782+Bh3ky@users.noreply.github.com>
2026-07-12 21:23:36 +0200

    feat: Implement user registration and authentication flow with protected routes

--- django-with-react/social-media-app/src/App.js
+++ django-with-react/social-media-app/src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import ProtectedRoute from './routes/ProtectedRoute';
+import Registration from './pages/Registration';
 import Home from './pages/Home';
 import './App.css';
 
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
+      <Route path="/" element={
+        <ProtectedRoute>
+          <Home />
+        </ProtectedRoute>
+      } />
+      <Route path='/login/' element={<div>Login</div>} />
+      <Route path='/register/' element={<Registration />} />
     </Routes>
   );
 }
